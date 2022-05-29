2ea689cb7ceb98e497bf1963af44b49a00012e93
Paulo Victor <paulo16xx@hotmail.com>
2022-05-29 09:40:09 -0300

    done: mario death logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
   mario.classList.add("jump");
@@ -6,5 +7,26 @@ const jump = () => {
     mario.classList.remove("jump");
   }, 500);
 };
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.left = `${marioPosition}px`;
+
+    mario.src = "./assets/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+
+    clearInterval(loop)
+  }
+}, 10);
 
 document.addEventListener("keydown", jump);
